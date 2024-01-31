b9a75738fa4c381b033016b435058d621289b551
Werner Glinka <werner@glinka.co>
2024-01-31 15:01:46 -0800

    converted plugin to es6

--- tests/index.js
+++ tests/index.js
@@ -2,35 +2,41 @@
 
 'use strict';
 
-const chai = require('chai');
-const metalsmith = require('metalsmith');
-const mdPartials = require('../lib');
-const fs = require('fs');
-const path = require('path');
-const expect = chai.expect;
+import * as chai from 'chai';
+import metalsmith from 'metalsmith';
+import mdPartials from '../lib/index.js';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
 
-const fixture = path.resolve.bind(path, __dirname, 'fixtures');
+/* eslint-disable no-underscore-dangle */
+const __dirname = dirname( fileURLToPath( import.meta.url ) );
 
-function file(_path) {
-  return fs.readFileSync(fixture(_path), 'utf8');
+const { expect } = chai;
+
+const fixture = path.resolve.bind( path, __dirname, 'fixtures' );
+
+function file( _path ) {
+  return fs.readFileSync( fixture( _path ), 'utf8' );
 }
 
-describe('metalsmith-markdown-partials', () => {
+describe( 'metalsmith-markdown-partials', () => {
 
-  it('should replace marker with markdown partial', done => {
+  it( 'should replace marker with markdown partial', done => {
 
-    metalsmith(fixture())
-      .use(mdPartials({
-          libraryPath: path.join(fixture(), `/src/md-partials/`),
-          fileSuffix: '.md',
-        }))
+    metalsmith( fixture() )
+      .use( mdPartials( {
+        libraryPath: path.join( fixture(), `/src/md-partials/` ),
+        fileSuffix: '.md',
+      } ) )
       .build( err => {
-        if (err) {
-          return done(err);
+        if ( err ) {
+          return done( err );
         }
-        expect(file('build/markdown.md')).to.be.eql(file('expected/final-markdown.md'));
-        
+        expect( file( 'build/markdown.md' ) ).to.be.eql( file( 'expected/final-markdown.md' ) );
+
         done();
-      });
-  });
-});
+      } );
+  } );
+} );
